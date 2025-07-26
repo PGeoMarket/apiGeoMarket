89f66f6590f1a159231aee57f3687edd1edc4f80
Stiven <almeidaibarra.stiven@gmail.com>
2025-07-26 14:59:52 -0500

    Add Complaint model, controller, and migration for complaints table

---
+++ app/Models/Complaint.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Complaint extends Model
+{
+    public function Rol(){
+        return $this->belongsTo('App\Models\Rol');
+    }
+}
