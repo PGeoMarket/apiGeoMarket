596edd530ca5fca69131f40f1861ed32532d6e77
PimPomUwU <daviparralopez@gmail.com>
2025-08-01 00:21:08 -0500

    Model(listas, scopes y relaciones), migration y controller

---
+++ database/migrations/2025_08_01_034030_create_publications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->varchar('titulo', 255);
+            $table->integer('precio');
+            $table->text('descripcion');
+            $table->longtext('imagen');
+            $table->boolean('visibilidad')->default(true);
+            $table->dateTime('fecha_publicacion')->useCurrent();
+            $table->dateTime('fecha_actualizacion')->nullable(); //modificacion de la publicacion
+            $table->unsignedBigInteger('seller_id');   // o seria user_id?
+            $table->unsignedBigInteger('category_id');    // id_categoria
+
+            $table->foreign('seller_id')->references('id')->on('sellers')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('category')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('publications');
+    }
+};
