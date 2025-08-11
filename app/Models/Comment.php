61468906de8d29d128f25d52f802b85e1878ba14
Camilo <baironramos959@gmail.com>
2025-08-11 16:17:18 -0500

    controlloer y scopes

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -15,4 +15,27 @@ class Comment extends Model
     {
         return $this->belongsTo(Publication::class, 'publication_id');
     }
+
+    public function scopeByUser($query, $userId)
+{
+    return $query->where('user_id', $userId);
+}
+
+// Filtrar por publicación
+public function scopeByPublication($query, $publicationId)
+{
+    return $query->where('publication_id', $publicationId);
+}
+
+// Filtrar por cantidad exacta de estrellas
+public function scopeByStars($query, $stars)
+{
+    return $query->where('valor_estrella', $stars);
+}
+
+// Buscar texto dentro del comentario
+public function scopeSearch($query, $keyword)
+{
+    return $query->where('texto', 'LIKE', "%{$keyword}%");
+}
 }
